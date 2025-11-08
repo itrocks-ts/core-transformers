810ad52ba5900e2cd8aae02ae66981fc537d0595
Baptiste Pillot <baptiste@pillot.fr>
2025-11-08 18:19:50 +0100

    Allow HTML container transformer to wrap objets returning HTML

--- src/container.ts
+++ src/container.ts
@@ -1,4 +1,5 @@
-import { HTML, setFormatTransformer } from '@itrocks/transformer'
+import { HTML }                 from '@itrocks/transformer'
+import { setFormatTransformer } from '@itrocks/transformer'
 
 export class HtmlContainer {
 
@@ -9,7 +10,20 @@ export class HtmlContainer {
 
 export function initContainerTransformers()
 {
-	setFormatTransformer(HTML, (value: string, askFor: HtmlContainer) =>
-		(askFor.container && askFor.mandatoryContainer) ? ('<div>' + value + '</div>') : value
-	)
+	setFormatTransformer(HTML, (value: any, askFor: HtmlContainer) => {
+		if (!(askFor.container && askFor.mandatoryContainer)) {
+			return value
+		}
+		if ((typeof(value) === 'object') && value.toString) {
+			const valueString = value.toString()
+			for (const propertyName in value) {
+				const propertyValue = value[propertyName]
+				if (((typeof propertyValue)[0] === 's') && (propertyValue === valueString)) {
+					value[propertyName] = '<div>' + propertyValue + '</div>'
+				}
+			}
+			return value
+		}
+		return '<div>' + value + '</div>'
+	})
 }
